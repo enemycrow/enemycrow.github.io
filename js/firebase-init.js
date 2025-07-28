7866e8e4b6867bee08ee9849c611d5757a7e3cbf
enemycrow <rodx.trean@gmail.com>
2025-07-27 23:20:04 -0400

    Revert "Switch firebase init to global style"

--- js/firebase-init.js
+++ js/firebase-init.js
@@ -1,19 +1,15 @@
-(function(window) {
-  // Configuración de Firebase
-  const firebaseConfig = {
-    apiKey: "YOUR_API_KEY",
-    authDomain: "YOUR_AUTH_DOMAIN",
-    projectId: "YOUR_PROJECT_ID",
-    storageBucket: "YOUR_STORAGE_BUCKET",
-    messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-    appId: "YOUR_APP_ID"
-  };
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/10.5.2/firebase-app.js';
+import { getFirestore } from 'https://www.gstatic.com/firebasejs/10.5.2/firebase-firestore.js';
 
-  // Inicializar Firebase
-  const app = firebase.initializeApp(firebaseConfig);
-  const db = firebase.firestore(app);
+// Rellena con las credenciales de tu proyecto de Firebase
+export const firebaseConfig = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_AUTH_DOMAIN",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_STORAGE_BUCKET",
+  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
+  appId: "YOUR_APP_ID"
+};
 
-  // Exponer las instancias globalmente
-  window.firebaseApp = app;
-  window.db = db;
-})(window);
+export const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
