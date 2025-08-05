c97eab4e330ff515dfad2b1d0801fcf00785f4d0
enemycrow <rodx.trean@gmail.com>
2025-08-05 09:09:18 -0400

    Smooth scroll to voice section

--- js/about.js
+++ js/about.js
@@ -22,6 +22,7 @@ document.addEventListener('DOMContentLoaded', function() {
                 const targetSection = document.getElementById(target);
                 if (targetSection) {
                     targetSection.classList.remove('hidden');
+                    targetSection.scrollIntoView({ behavior: 'smooth', block: 'start' });
                 }
             });
         });
