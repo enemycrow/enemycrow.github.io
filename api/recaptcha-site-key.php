f4e8c3ed06babf41b05d2bbd89ca35211006f0c2
enemycrow <rodx.trean@gmail.com>
2025-09-09 18:14:39 -0300

    Fetch reCAPTCHA site key from config

---
+++ api/recaptcha-site-key.php
@@ -0,0 +1,17 @@
+<?php
+declare(strict_types=1);
+
+header('Content-Type: application/json');
+
+try {
+    $config = require __DIR__ . '/config.php';
+    $siteKey = $config['recaptcha_site_key'] ?? '';
+    if ($siteKey === '') {
+        throw new RuntimeException('Missing reCAPTCHA site key');
+    }
+
+    echo json_encode(['siteKey' => $siteKey]);
+} catch (Throwable $e) {
+    http_response_code(500);
+    echo json_encode(['error' => 'Could not load reCAPTCHA site key']);
+}
